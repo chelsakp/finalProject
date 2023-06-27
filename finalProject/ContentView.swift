9ebe9ebb9a08761c3230ac36e1bdd7d820f2084a
Rachel Onwu <rachelonwu@gmail.com>
2023-06-27 15:55:08 -0400

    yes ma'am

--- finalProject/ContentView.swift
+++ finalProject/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hi!")
-            //chelsey needs help!
-        }
+        ZStack{
+            
+            VStack(alignment: .leading, spacing:20){
+                Image("banana")
+                    .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                    .frame(width: 50.0, height: 50.0)
+                
+                HStack{
+                    Image("logo")
+                        .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                        .frame(width: 350.0, height: 400.0)
+                        .position(x:170, y:80)
+                   /* VStack(alignment:){
+                     
+                    }*/
+                    
+                }//end of HStack
+            }//end of VStack
+        }//end of ZStack
         .padding()
     }//jasssss
 }
