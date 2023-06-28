745a7b6409113136c889ab1e3b5523e696bc0586
Chelsey Akpaka <chelseyoakpaka@gmail.com>
2023-06-28 15:56:49 -0400

    hi

--- ingred1.swift
+++ ingred1.swift
@@ -9,13 +9,55 @@ import SwiftUI
 
 struct ingred1: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-      
-    }
-}
+        VStack(alignment: .leading, spacing: 20.0){
+            
+            Image("Image")
+                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                .frame(width: 100.0, height: 100.0)
+                .position(x: 180, y: 80)
+            
+            Image("Gnocchi")
+                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                .frame(width: 110.0, height: 110.0)
+                .position(x:179,y:120)
+                
+                
+            Text("Potato Gnocchi")
+                .font(.title3)
+                .fontWeight(.bold)
+                .position(x:180,y:90)
+            
+            Text("Cripsy Hashbrown")
+                .position(x:179, y: 170)
+                .fontWeight(.bold)
+                .font(.title3)
+                
+        
+            Image("Hashbrown")
+                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                .frame(width: 120.0, height: 120.0)
+                .position(x: 179, y:-20)
+            
+            Image("Mashed")
+                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                .frame(width: 120.0, height: 120.0)
+                .position(x:179, y:50)
+            
+            Text("Mashed Potatoes")
+                .position(x:179, y:30)
+                .fontWeight(.bold)
+                .font(.title3)
+            
+            
+    
+            
+            
 
-struct ingred1_Previews: PreviewProvider {
-    static var previews: some View {
-        ingred1()
+        }
+    }
+    struct ingred1_Previews: PreviewProvider {
+        static var previews: some View {
+            ingred1()
+        }
     }
 }
