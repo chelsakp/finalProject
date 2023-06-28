8c4fab8bcc5e8fb435b7509b4fd5e1ee4c7747cc
Rachel Onwu <rachelonwu@gmail.com>
2023-06-28 11:01:59 -0400

    day7

--- finalProject/ContentView.swift
+++ finalProject/ContentView.swift
@@ -9,28 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            
-            VStack(alignment: .leading, spacing:20){
-                Image("banana")
-                    .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
-                    .frame(width: 50.0, height: 50.0)
-                
-                HStack{
-                    Image("logo")
-                        .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
-                        .frame(width: 350.0, height: 400.0)
-                        .position(x:170, y:80)
-                   /* VStack(alignment:){
-                     
-                    }*/
+    ZStack{
+       
+     
+            Image("logo")
+                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                .frame(width: 350.0, height: 400.0)
+                .position(x:190, y:100)
+        /*VStack(alignment: .leading, spacing:20){
+            Image("banana")
+                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                .frame(width: 50.0, height: 50.0)*/
+        VStack(alignment: .center, spacing: 20.0) {
                     
-                }//end of HStack
+            Image("banana")
+                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 150.0, height: 150.0)
+                .cornerRadius(15)
+                
+            
+                    
+        HStack(spacing: 20.0) {
+            Text("banana")
+                .font(.title)
+                .multilineTextAlignment(.leading)
+            }
+
+            }
+                    
+                    
+              
             }//end of VStack
         }//end of ZStack
-        .padding()
+     
     }//jasssss
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
