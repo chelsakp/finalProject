954a21b9777dedf81ef63243ca836d265358e990
Ally Paul <aepaul0356@gmail.com>
2023-06-29 10:47:31 -0400

    hello

--- ingred2.swift
+++ ingred2.swift
@@ -9,12 +9,35 @@ import SwiftUI
 
 struct ingred2: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image("milk")
+                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                .frame(width: 100.0, height: 140.0)
+                .border(Color.blue,
+                        width:3)
+            Text("Milk")
+            
+            
+            Image("iceCream")
+                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                .frame(width: 100.0, height: 100.0)
+            Text("Icecream")
+            
+            
+            Image("strawberrySundaes")
+                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                .frame(width: 100.0, height: 100.0)
+            Text("Strawberry Sundae")
+            
+            
+            Image("bananaSplit")
+            Text("Classic Banana Split")
+        }
     }
-}
-
-struct ingred2_Previews: PreviewProvider {
-    static var previews: some View {
-        ingred2()
+    
+    struct ingred2_Previews: PreviewProvider {
+        static var previews: some View {
+            ingred2()
+        }
     }
 }
